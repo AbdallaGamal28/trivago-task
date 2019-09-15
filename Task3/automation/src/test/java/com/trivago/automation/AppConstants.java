df92c463a4ccd9697402d94b890c6744558858e8
Abdalla Gamal <52201436+AbdallaGamal28@users.noreply.github.com>
2019-09-15 22:00:39 +0400

    adding ui automation task code

---
+++ Task3/automation/src/test/java/com/trivago/automation/AppConstants.java
@@ -0,0 +1,20 @@
+package com.trivago.automation;
+
+public class AppConstants {
+
+	public static final String POSITIONS_URL = "https://company.trivago.com/open-positions/";
+	public static final String DRIVER_PATH = "D:\\chromedriver_win32\\chromedriver.exe";
+	public static final String JOB_TITLE = "//*[@id=\"content\"]/section[2]/div/div/div/h1";
+	public static final String JOB_FAMILY = "//*[@id=\"job_family\"]";
+	public static final String EXPERIENCE_LEVEL = "//*[@id=\"content\"]/section[3]/div/div/div[2]/p[2]/span";
+	public static final String LOCATION = "//*[@id=\"content\"]/section[3]/div/div/div[2]/p[3]/span";
+	public static final String LANGUAGE = "//*[@id=\"content\"]/section[3]/div/div/div[2]/p[4]/span";
+	public static final String TEST_PAGE = "https://company.trivago.com/jobs/r0001914/";
+	public static final String APPLY_BTN1 = "//*[@id='content']/section[3]/div/div/div[2]/button";
+	public static final String APPLY_BTN2 = "//*[@id=\"content\"]/section[3]/div/div/div[4]/button";
+	public static final String WHAT_YOU_DO = "//*[@id=\"content\"]/section[3]/div/div/div[3]/p[11]/span/span/span/span/b";
+	public static final String WHAT_YOU_NEED = "//*[@id=\"content\"]/section[3]/div/div/div[3]/p[12]/span/span/span/span[2]/b";
+	public static final String WHAT_WE_LOVE = "//*[@id=\"content\"]/section[3]/div/div/div[3]/p[14]/span/span/span/span/b";
+	public static final String POSITIONS_TABLE = "//*[@id='list-jobs']/table[1]/tbody";
+	public static final String POSITIONS_TABLE_ROW =   "//*[@id='list-jobs']/table[1]/tbody/tr";
+}
---
+++ Task3/automation/src/test/java/com/trivago/automation/JobProfileTestCase.java
@@ -0,0 +1,80 @@
+package com.trivago.automation;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static com.trivago.automation.AppConstants.*;
+
+public class JobProfileTestCase {
+
+	private WebDriver driver;
+
+	{
+		System.setProperty("webdriver.chrome.driver", AppConstants.DRIVER_PATH);
+		driver = new ChromeDriver();
+	}
+
+	public void wait(int seconds) {
+		driver.manage().timeouts().implicitlyWait(seconds, TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void testJobProfile() throws Exception {
+		driver.get(POSITIONS_URL);
+		wait(10);
+		WebElement mytable = driver.findElement(By.xpath(POSITIONS_TABLE));
+		List<WebElement> elements = mytable.findElements(By.tagName("tr"));
+		System.out.println("Opening Positions Count = " + elements.size());
+		
+		for (int i = 1; i < elements.size(); i++) {
+			WebElement position = driver.findElement(By.xpath(POSITIONS_TABLE_ROW +  "[" + i + "]"));
+			System.out.println("Check the following Job Profile ::: " + position.getText());
+			position.click();
+			wait(5);
+			checkJobFields();
+			driver.navigate().back();
+		}
+	}
+
+	void checkJobFields() {
+		String jobTitle = driver.findElement(By.xpath(JOB_TITLE)).getTagName();
+		String jobFamily = driver.findElement(By.xpath(JOB_FAMILY)).getText();
+		String jobLanguage = driver.findElement(By.xpath(LANGUAGE)).getText();
+		String jobLocation = driver.findElement(By.xpath(LOCATION)).getText();
+		String experienceLevel = driver.findElement(By.xpath(EXPERIENCE_LEVEL)).getText();
+		WebElement applyBtn1 = driver.findElement(By.xpath(APPLY_BTN1));
+		WebElement applyBtn2 = driver.findElement(By.xpath(APPLY_BTN2));
+		String whatYouDo = driver.findElement(By.xpath(WHAT_YOU_DO)).getTagName();
+		String whatYouNeed = driver.findElement(By.xpath(WHAT_YOU_NEED)).getTagName();
+		String whatWeLove = driver.findElement(By.xpath(WHAT_WE_LOVE)).getTagName();
+
+		System.out.println("Job Title tag name  :: " + jobTitle);
+		System.out.println("Job Family :: " + jobFamily);
+		System.out.println("Job Language :: " + jobLanguage);
+		System.out.println("Job Location :: " + jobLocation);
+		System.out.println("Experience Level :: " + experienceLevel);
+		System.out.println("Apply Button 1 :: " + applyBtn1);
+		System.out.println("Apply Button 2 :: " + applyBtn2);
+		System.out.println("What you do tag name  :: " + whatYouDo);
+		System.out.println("What you need tag name  :: " + whatYouNeed);
+		System.out.println("What we love tag name : " + whatWeLove);
+
+		assertEquals(jobFamily.length() > 0, true);
+		assertEquals(jobLanguage.length() > 0, true);
+		assertEquals(jobLocation.length() > 0, true);
+		assertEquals(experienceLevel.length() > 0, true);
+		assertNotNull(applyBtn1);
+		assertNotNull(applyBtn2);
+		assertEquals(jobTitle, "h1");
+		assertEquals(whatYouDo, "b");
+		assertEquals(whatYouNeed, "b");
+		assertEquals(whatWeLove, "b");
+	}
+}
